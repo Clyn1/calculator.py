07f28d0539614c63a257ed56ed838b8aa8ef7b94
Clyn1 <ochiengclyntono@gmail.com>
2025-03-11 21:20:32 +0300

    simple calcular program

---
+++ index.py
@@ -0,0 +1,23 @@
+#Step 1: Take user input 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the Operation(+,-,*,/): ")
+
+#Step 2: Perform the operation
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    if num2 == 0:
+      result = "Error! Division by zero is not allowed."
+    else:
+      result = num1 / num2
+
+else:
+    result = "Invalid operator!"
+
+#Step 3: Display the result
+print(f"{num1} {operator} {num2} = {result}")
